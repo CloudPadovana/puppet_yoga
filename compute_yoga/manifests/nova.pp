2bbde94932082e53975e3a916a6c2ab0de40db1f
fanzago <federica.fanzago@cern.ch>
2022-05-23 11:43:58 +0200

    removed old train entries for cld-dfa-gpu-03

--- compute_yoga/manifests/nova.pp
+++ compute_yoga/manifests/nova.pp
@@ -237,20 +237,20 @@ compute_yoga::nova::do_config { 'nova_enable_proxy_headers_parsing': conf_file =
 
 # GPU specific setting and some setting for better performance for SSD disk for cld-dfa-gpu-03
  if ($::mgmtnw_ip == "192.168.60.83") {
-  compute_yoga::nova::do_config { 'pci_passthrough_whitelist': conf_file => '/etc/nova/nova.conf', section => 'pci', param => 'passthrough_whitelist', value => $compute_train::params::pci_passthrough_whitelist, }
+  compute_yoga::nova::do_config { 'pci_passthrough_whitelist': conf_file => '/etc/nova/nova.conf', section => 'pci', param => 'passthrough_whitelist', value => $compute_yoga::params::pci_passthrough_whitelist, }
 
    compute_yoga::nova::do_config_list { "pci_alias":
            conf_file => '/etc/nova/nova.conf',
            section   => 'pci',
            param     => 'alias',
-           values    => [ "$compute_train::params::pci_A4000_VGA", "$compute_train::params::pci_A4000_SND" ],
+           values    => [ "$compute_yoga::params::pci_A4000_VGA", "$compute_yoga::params::pci_A4000_SND" ],
          }
 
    compute_yoga::nova::do_config_list { "preallocate_images":
            conf_file => '/etc/nova/nova.conf',
            section   => 'DEFAULT',
            param     => 'preallocate_images',
-           values    => [ "$compute_train::params::nova_preallocate_images"   ],
+           values    => [ "$compute_yoga::params::nova_preallocate_images"   ],
          }
 
 
