f3577a0283ecb64538fed90b7b1943de6a8930e5
root <root@cld-config.cloud.pd.infn.it>
2024-01-10 09:43:04 +0100

    Nuova versione degli rpms di bareos

--- compute_yoga/manifests/bacula.pp
+++ compute_yoga/manifests/bacula.pp
@@ -49,13 +49,13 @@ case $operatingsystemrelease {
               package {"bareos-common":
                  provider => "rpm",
                  ensure   => installed,
-                 source => "https://download.bareos.org/current/EL_9/x86_64/bareos-common-22.1.2~pre4.c37b33136-104.el9.x86_64.rpm",
+                 source => "https://download.bareos.org/current/EL_9/x86_64/bareos-common-23.0.1~pre26.caf6d583a-35.el9.x86_64.rpm",
                       }  
  
               package {"bareos-filedaemon":
                  provider => "rpm",
                  ensure   => installed,
-                 source => "https://download.bareos.org/current/EL_9/x86_64/bareos-filedaemon-22.1.2~pre4.c37b33136-104.el9.x86_64.rpm",
+                 source => "https://download.bareos.org/current/EL_9/x86_64/bareos-filedaemon-23.0.1~pre26.caf6d583a-35.el9.x86_64.rpm",
                  require => Package["bareos-common"],
                       }  
   
