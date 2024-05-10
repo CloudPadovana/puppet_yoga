c0f1d367a5dcb6ee6017d5e6a875d06419ec29d0
traldi <sergio.traldi@pd.infn.it>
2024-05-10 12:35:10 +0200

    Change the bareos rpm endpoint and version for Centos Stream 8 and Almalinux 9

--- compute_yoga/manifests/bacula.pp
+++ compute_yoga/manifests/bacula.pp
@@ -23,13 +23,13 @@ case $operatingsystemrelease {
               package {"bareos-common":
                  provider => "rpm",
                  ensure   => installed,
-                 source => "https://download.bareos.org/bareos/release/19.2/CentOS_8/x86_64/bareos-common-19.2.7-2.el8.x86_64.rpm",
+                 source => "http://cld-config.cloud.pd.infn.it:8090/bareos/bareos-common-19.2.7-2.el8.x86_64.rpm",
                       }  
  
               package {"bareos-filedaemon":
                  provider => "rpm",
                  ensure   => installed,
-                 source => "https://download.bareos.org/bareos/release/19.2/CentOS_8/x86_64/bareos-filedaemon-19.2.7-2.el8.x86_64.rpm",
+                 source => "http://cld-config.cloud.pd.infn.it:8090/bareos/bareos-filedaemon-19.2.7-2.el8.x86_64.rpm",
                  require => Package["bareos-common"],
                       }  
   
@@ -49,13 +49,13 @@ case $operatingsystemrelease {
               package {"bareos-common":
                  provider => "rpm",
                  ensure   => installed,
-                 source => "http://cld-config.cloud.pd.infn.it:8090/bareos/bareos-common-23.0.2~pre53.e52dbae7a-53.el9.x86_64.rpm",
+                 source => "http://cld-config.cloud.pd.infn.it:8090/bareos/bareos-common-19.2.7-2.el8.x86_64.rpm",
                       }  
  
               package {"bareos-filedaemon":
                  provider => "rpm",
                  ensure   => installed,
-                 source => "http://cld-config.cloud.pd.infn.it:8090/bareos/bareos-filedaemon-23.0.2~pre53.e52dbae7a-53.el9.x86_64.rpm",
+                 source => "http://cld-config.cloud.pd.infn.it:8090/bareos/bareos-filedaemon-19.2.7-2.el8.x86_64.rpm",
                  require => Package["bareos-common"],
                       }  
   
